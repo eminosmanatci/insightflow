0e13830b0804865b0bfcde23a10a6dd00fabdddb
eminosmanatci <atcieminosman@gmail.com>
2026-07-20 15:54:47 +0300

    fix: remove hardcoded api keys and use environment variables

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,6 @@
-from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
 
-class Settings(BaseSettings):
-    PROJECT_NAME: str = "InsightFlow"
-    DATABASE_URL: str = "postgresql://insight_admin:secret_password123@db:5432/insightflow_prod"
-    
-    # Groq API Key
-    GROQ_API_KEY: str = "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" 
+load_dotenv()
 
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
+LLAMA_API_KEY = os.getenv("LLAMA_API_KEY")
